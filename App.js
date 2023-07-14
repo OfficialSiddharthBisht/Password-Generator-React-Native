71ef4d9e3a66b2e7d068dc894b68113b186e2d24
Siddharth Bisht <officialsiddharthbisht@gmail.com>
2023-07-14 22:16:45 +0530

    feat: form validation using yup

--- App.js
+++ App.js
@@ -1,6 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// Form Validation
+import * as Yup from 'yup';
+
+const PasswordSchema = Yup.object().shape({
+  passwordLength : Yup.number()
+  .min(8,"Should be min of 4 characters")
+  .max(16,"Cannot be more then 16 characters")
+  .required("This is a required field")
+})
 export default function App() {
   return (
     <View style={styles.container}>
