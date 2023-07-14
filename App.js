78e1cc25022fbe725ec46bfb3be6882bafcde14a
Siddharth Bisht <officialsiddharthbisht@gmail.com>
2023-07-14 22:42:55 +0530

    feat: complete all method logic

--- App.js
+++ App.js
@@ -16,17 +16,46 @@ export default function App() {
   const [isPassGenerated, setIsPassGenerated] = useState(false);
   const [lowerCase,setLowerCase] = useState(true);
   const [upperCase,setUpperCase] = useState(false);
-  const [numbers,useNumbers] = useState(false);
-  const [symbols,useSymbols] = useState(false);
+  const [numbers,setNumbers] = useState(false);
+  const [symbols,setSymbols] = useState(false);
 
   const generatePasswordString = (passwordLength) =>{
-    // 
+    let characterList = '';
+    const upperCaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const lowerCaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const digitChars = "0123456789";
+    const specialChars = "!@#$%^&*()_+-=";
+    if(upperCase){
+      characterList += upperCaseChars;
+    }
+    if(lowerCase){
+      characterList += lowerCaseChars;
+    }
+    if(numbers){
+      characterList += digitChars;
+    }
+    if(symbols){
+      characterList += symbols;
+    }
+    const passwordResult = createPassword(characterList,passwordLength);
+    setPassword(passwordResult)
+    setIsPassGenerated(true);
   }
-  const createPassword = (characters, passwordString) =>{
-    // 
+  const createPassword = (characters, passwordLength) =>{
+    let result = "";
+    for(let i = 0; i < passwordLength; i++){
+      const characterIndex = Math.round(Math.random() * characters.length);
+      result += characters.charAt(characterIndex);
+    }
+    return result;
   }
   const resetPasswordState = () =>{
-    // 
+    setPassword('');
+    setIsPassGenerated(false);
+    setLowerCase(true);
+    setUpperCase(false);
+    setNumbers(false)
+    setSymbols(false);
   }
 
   return (
